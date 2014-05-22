18afe6c695dc6aa479bf5a8c2c7ece5fe9fcff56
Luiz Branco <me@luizbranco.com>
2014-05-21 23:20:08 -0300

    Add neighbors coordinates method

--- hex/hex.go
+++ hex/hex.go
@@ -1,19 +1,35 @@
 package hex
 
+// Hexagon (6-sided polygons) using cubic coordinates
 type Hex struct {
   x, y, z int
 }
 
+// Converts hexagon cubic coordinates to axial ones
 func (h *Hex) AxialCoordinates() (r, q int) {
   r = h.z
   q = h.x
   return
 }
 
+// Creates an hexagon cubic coordinates from axial coordinates
 func NewFromAxialCoordinates(r, q int) *Hex {
   h := new(Hex)
   h.x = q
   h.z = r
   h.y = -q-r
   return h
+}
+
+// Finds adjacent hexagons
+func (h *Hex) NeighborCoordinates() ([6][3]int) {
+  neighbors := [6][3]int{
+    {+1, -1,  0}, {+1,  0, -1}, { 0, +1, -1},
+    {-1, +1,  0}, {-1,  0, +1}, { 0, -1, +1},
+  }
+  var coords [6][3]int
+  for i, value := range neighbors {
+    coords[i] = [3]int{h.x + value[0], h.y + value[1], h.z + value[2]}
+  }
+  return coords
 }
--- hex/hex_test.go
+++ hex/hex_test.go
@@ -6,10 +6,10 @@ func TestAxialCoordinates(t *testing.T) {
   h := Hex{x: 0, y: 1, z: -1}
   r, q := h.AxialCoordinates()
   if q != h.x {
-    t.Error("Expected q to equal x(0), got", q)
+    t.Error("Expected q to equal", h.x ,"got", q)
   }
   if r != h.z {
-    t.Error("Expected r to equal z(-1), got", r)
+    t.Error("Expected r to equal", h.z, "got", r)
   }
 }
 
@@ -17,12 +17,24 @@ func TestNewFromAxialCoordinates(t *testing.T) {
   r, q := -1, 0
   h := NewFromAxialCoordinates(r, q)
   if h.x != q {
-    t.Error("Expected x to equal q(0), got", h.x)
+    t.Error("Expected x to equal", q ,"got", h.x)
   }
   if h.z != r {
-    t.Error("Expected z to equal r(-1), got", h.z)
+    t.Error("Expected z to equal", r ,"got", h.z)
   }
   if h.y != 1 {
-    t.Error("Expected y to equal q(1), got", h.y)
+    t.Error("Expected y to equal", 1 ,"got", h.y)
+  }
+}
+
+func TestNeighborCoordinates(t *testing.T) {
+  h := Hex{x: 0, y: 1, z: -1}
+  coords := [6][3]int{
+    {+1,  0, -1}, {+1, +1, -2}, { 0, +2, -2},
+    {-1, +2, -1}, {-1, +1,  0}, { 0,  0,  0},
+  }
+  neighbors := h.NeighborCoordinates()
+  if coords != neighbors {
+    t.Error("Expected", coords, "got ", neighbors)
   }
 }
